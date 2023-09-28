93f849aa25a7faf028467981d55e9abc1b7e56f0
Piotr Kowalecki <piotr.kowalecki@ug.edu.pl>
2023-09-28 20:04:42 +0000

    initial commit

---
+++ src/main/java/pl/kowalecki/dietplanner/DietPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.kowalecki.dietplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kowalecki/dietplanner/DietPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kowalecki.dietplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
