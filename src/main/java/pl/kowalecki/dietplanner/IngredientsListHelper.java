24992fc75db48a62122eceba9936ea349d28e7ca
Piotr Kowalecki <piotr.kowalecki@ug.edu.pl>
2024-03-09 17:30:54 -0100

    - Added ingredient amount sum

--- src/main/java/pl/kowalecki/dietplanner/IngredientsListHelper.java
+++ src/main/java/pl/kowalecki/dietplanner/IngredientsListHelper.java
@@ -12,13 +12,12 @@ public class IngredientsListHelper {
         for (Ingredient newIngredient: ingredients){
             Ingredient newIngredientToPut = new Ingredient();
             String ingredientNameKey = newIngredient.getName() + newIngredient.getIngredientUnit().getShortName();
-            String measurementNameKey = newIngredient.getName() + newIngredient.getMeasurementType().getMeasurementName();
             if (ingredientMap.containsKey(ingredientNameKey)){
                 Ingredient existingIngredient = ingredientMap.get(ingredientNameKey);
                 if(newIngredient.getName().equals(existingIngredient.getName())){
                     //Tutaj lecimy z jednostkami danego składnika
                     if (newIngredient.getIngredientUnit().equals(existingIngredient.getIngredientUnit())){
-                        existingIngredient.setIngredientAmount(existingIngredient.sumTotalAmount(newIngredient.getIngredientAmount(),existingIngredient.getIngredientAmount()));
+                        existingIngredient.setIngredientAmount(existingIngredient.sumTotalAmount(newIngredient.getIngredientAmount()*multiplier,existingIngredient.getIngredientAmount()));
                         existingIngredient.setIngredientUnit(existingIngredient.getIngredientUnit());
                     }
                     //Tutaj lecimy z rodzajem danego składnika
@@ -34,26 +33,6 @@ public class IngredientsListHelper {
                 newIngredient.setMeasurementValue(newIngredient.getMeasurementValue()*multiplier);
                 ingredientMap.put(ingredientNameKey, newIngredient);
             }
-
-//            if (ingredientMap.containsKey(measurementNameKey)){
-//                Ingredient existingMeasurement = ingredientMap.get(measurementNameKey);
-//                if(newIngredient.getName().equals(existingMeasurement.getName())){
-//                    if (newIngredient.getMeasurementType().equals(existingMeasurement.getMeasurementType())){
-//                        existingMeasurement.setMeasurementValue(existingMeasurement.sumTotalAmount(newIngredient.getMeasurementValue(),existingMeasurement.getMeasurementValue()));
-//                        existingMeasurement.setMeasurementType(existingMeasurement.getMeasurementType());
-//                        ingredientMap.put(measurementNameKey, existingMeasurement);
-//                    }else {
-//                        ingredientMap.put(measurementNameKey, existingMeasurement);
-//                    }
-//                }
-//            }else{
-//                newIngredient.setMeasurementValue(newIngredient.getMeasurementValue()*multiplier);
-//                ingredientMap.put(measurementNameKey, newIngredient);
-//            }
-
-
-
-
         }
 
         //Sortujemy mapkę, żeby dostać listę posortowaną alfabetycznie.
@@ -65,17 +44,4 @@ public class IngredientsListHelper {
         return new ArrayList<>(sortedMap.values());
     }
 
-//    private static void checkAndAddExistingMeasurement(Ingredient ingredient, Ingredient existingIngredient) {
-//        String totalMeasurement = ingredient.getIngredientMeasurement();
-//
-//        IngredientMeasurement measurementAmount = Ingredient.getOnlyDoubleFromMeasurement(existingIngredient.getIngredientMeasurement());
-//        IngredientMeasurement newMeasurement = Ingredient.getOnlyDoubleFromMeasurement(totalMeasurement);
-//
-//        if(measurementAmount.getIngredientMeasurementUnit().equals(newMeasurement.getIngredientMeasurementUnit())){
-//            existingIngredient.setIngredientMeasurement(existingIngredient.sumTotalAmount(measurementAmount.getIngredientMeasurementAmount(),newMeasurement.getIngredientMeasurementAmount()) +" "+ measurementAmount.getIngredientMeasurementUnit());
-//        }else{
-//            existingIngredient.setIngredientMeasurement(measurementAmount.getIngredientMeasurementAmount() +" "+ measurementAmount.getIngredientMeasurementUnit() + " oraz "
-//                    + newMeasurement.getIngredientMeasurementAmount() +" "+ newMeasurement.getIngredientMeasurementUnit());
-//        }
-//    }
 }
