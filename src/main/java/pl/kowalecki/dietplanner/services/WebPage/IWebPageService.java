a88103771bcb96e55d061c48b8efc860cdffcda1
pkowalecki <pkowalecki@gmail.com>
2024-12-05 14:49:14 +0100

    - tmep. background image - added new ftlh for macros - added webpageconfig which one includes pagedata

--- src/main/java/pl/kowalecki/dietplanner/services/WebPage/IWebPageService.java
+++ src/main/java/pl/kowalecki/dietplanner/services/WebPage/IWebPageService.java
@@ -1,9 +1,13 @@
 package pl.kowalecki.dietplanner.services.WebPage;
 
+import freemarker.template.SimpleSequence;
 
-
+import java.util.ArrayList;
 import java.util.Map;
 
 public interface IWebPageService {
     Map<String, String> addMessageToPage(MessageType type, String message);
+    void addError(String pole, String error);
+    Map<String, ArrayList<String>> getErrors();
+    SimpleSequence getErrorsAsList();
 }
--- src/main/java/pl/kowalecki/dietplanner/services/WebPage/WebPageService.java
+++ src/main/java/pl/kowalecki/dietplanner/services/WebPage/WebPageService.java
@@ -1,5 +1,6 @@
 package pl.kowalecki.dietplanner.services.WebPage;
 
+import freemarker.template.SimpleSequence;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +17,8 @@ import java.util.Map;
 @Slf4j
 @AllArgsConstructor
 public class WebPageService implements IWebPageService {
+
+    private final Map<String, ArrayList<String>> errors = new HashMap<String, ArrayList<String>>();
 
     private Map<String, String> setMsg(MessageType type, String message) {
         Map<String, String> messageData = new HashMap<>();
@@ -27,5 +31,30 @@ public class WebPageService implements IWebPageService {
     @Override
     public Map<String, String> addMessageToPage(MessageType type, String message) {
         return setMsg(type, message);
+    }
+
+    @Override
+    public void addError(final String pole, final String error) {
+        ArrayList<String> lista = errors.get(pole);
+        if (lista == null) lista = new ArrayList<String>();
+        lista.add(error);
+        errors.put(pole, lista);
+    }
+
+    @Override
+    public Map<String, ArrayList<String>> getErrors() {
+        return errors;
+    }
+
+    @Override
+    public SimpleSequence getErrorsAsList() {
+        SimpleSequence ss = new SimpleSequence();
+        for (String key : errors.keySet()) {
+            Map<String, Object> mapka = new HashMap<String, Object>();
+            mapka.put("name", key);
+            mapka.put("messages", errors.get(key));
+            ss.add(mapka);
+        }
+        return ss;
     }
 }
